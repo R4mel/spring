f2d3aa42a32300bedf3746b9776fa2a15424f33b
Ramel <dev_20@naver.com>
2023-08-17 00:23:54 +0900

    spring

--- src/main/java/hello/spring/controller/MemberController.java
+++ src/main/java/hello/spring/controller/MemberController.java
@@ -4,8 +4,7 @@ import hello.spring.domain.Member;
 import hello.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MemberController {
@@ -18,18 +17,18 @@ public class MemberController {
     // 의존성 주입에는 필드 주입, Setter 주입, 생성자 주입이 있다.
     // 의존관계가 실행중에 동적으로 변하는 경우는 거의 없으므로 생성자 주입을 권장한다.
 
-    @GetMapping("/member/new")
+    @RequestMapping(value="/members/new", method = {RequestMethod.GET})
     public String createForm(){
         return "members/createMemberForm";
     }
 
-    @PostMapping("/members/new")
+    @RequestMapping(value="/members/new", method = {RequestMethod.POST})
     public String create(MemberForm form){
         Member member = new Member();
         member.setName(form.getName());
 
         memberService.join(member);
 
-        return "redirect:/";
+            return "redirect:/";
     }
 }

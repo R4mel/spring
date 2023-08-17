8e009ed3847710287204b5496c4d2df9532ece38
Ramel <dev_20@naver.com>
2023-08-17 14:43:59 +0900

    spring

--- src/main/java/hello/spring/controller/MemberController.java
+++ src/main/java/hello/spring/controller/MemberController.java
@@ -4,7 +4,10 @@ import hello.spring.domain.Member;
 import hello.spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -30,5 +33,12 @@ public class MemberController {
         memberService.join(member);
 
             return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
